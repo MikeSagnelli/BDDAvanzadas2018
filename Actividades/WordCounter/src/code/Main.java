909556032bd5b4e90296e259f2e4cabc7326da51
MikeSagnelli <ing.sagnelli@gmail.com>
2018-01-31 19:30:09 -0600

    Solved first activity, to count how many times does a word appears in a text file.

---
+++ Actividades/WordCounter/src/code/Main.java
@@ -0,0 +1,12 @@
+package code;
+
+// Class for running and testing
+public class Main {
+
+	public static void main(String[] args) {
+		// Create an instance of our solution class
+		Counter counter = new Counter("C:\\Users\\ingsa\\Desktop\\test.txt");
+		counter.printOutput(); // Run the implemented method
+	}
+
+}
